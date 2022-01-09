06e00130027c182c72f774c65dfb45c1b11e44ea
Lakshya Raikwal <lakshya.raikwal@giis.edu.sg>
2022-01-09 16:16:51 +0800

    Add files via upload

---
+++ blink.py
@@ -0,0 +1,72 @@
+import cv2
+import cvzone  # == 1.5.4
+from cvzone.FaceMeshModule import FaceMeshDetector
+from cvzone.PlotModule import LivePlot
+
+cap = cv2.VideoCapture("Video.mp4")
+detector = FaceMeshDetector(maxFaces=1)
+plotY = LivePlot(640, 360, [20, 50], invert=True)
+
+idList = [22, 23, 24, 26, 110, 157, 158, 159, 160, 161, 130, 243]
+ratioList = []
+
+blinkCounter = 0
+counter = 0
+
+color = (255, 0, 0)
+
+while True:
+
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+    success, img = cap.read()
+
+    img, faces = detector.findFaceMesh(img, draw=False)
+
+    if faces:
+        face = faces[0]
+        for id in idList:
+            cv2.circle(img, tuple(face[id]), 5, color, cv2.FILLED)
+
+        leftUp = face[159]
+        leftDown = face[23]
+        leftLeft = face[130]
+        leftRight = face[243]
+        lengthVer, _ = detector.findDistance(leftUp, leftDown)
+        lengthHor, _ = detector.findDistance(leftLeft, leftRight)
+
+        cv2.line(img, tuple(leftUp), tuple(leftDown), (0, 200, 0), 3)
+        cv2.line(img, tuple(leftLeft), tuple(leftRight), (0, 200, 0), 3)
+
+        ratio = int((lengthVer / lengthHor) * 100)
+        ratioList.append(ratio)
+
+        if len(ratioList) > 3:
+            ratioList.pop(0)
+        ratioAvg = sum(ratioList) / len(ratioList)
+
+        if ratioAvg < 35 and counter == 0:
+            blinkCounter += 1
+            color = (255, 0, 255)
+            counter = 1
+
+        if counter != 0:
+            counter += 1
+            if counter > 10:
+                counter = 0
+                color = (255, 0, 0)
+
+        cvzone.putTextRect(img, f"Blink Count: {blinkCounter}", (50, 100), colorR=color)
+
+        imgPLot = plotY.update(ratioAvg, color)
+        img = cv2.resize(img, (640, 360))
+        imgStack = cvzone.stackImages([img, imgPLot], 2, 1)
+
+    else:
+        img = cv2.resize(img, (640, 360))
+        imgStack = cvzone.stackImages([img, img], 1, 1)
+
+    cv2.imshow("Stacked", imgStack)
+
+    cv2.waitKey(1)
